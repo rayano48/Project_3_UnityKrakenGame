ec041bac5a4646cd550ece225eda7e3360a7c54d
Rayan <ryanbud348@gmail.com>
2023-11-13 14:15:43 -0600

    added trigger, script, and removed unnecessary objects

--- Assets/Scripts/KrakenTests.cs
+++ Assets/Scripts/KrakenTests.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class KrakenTests : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] string _whatToSay;
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
         
     }
